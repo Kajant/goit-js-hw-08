6d870480cf4d068f55f00473d8e0829532708b6d
Kajetan Antoszkiewicz <wet.kajetan.antoszkiewicz@gmail.com>
2023-05-28 18:00:46 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+'use strict';
+import throttle from "lodash.throttle";
 
+const feedbackForm = document.querySelector('form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+feedbackForm.addEventListener('input', throttle(event => {
+    const feedbackContent = { email: emailInput.value, message: messageInput.value };
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackContent));
+}, 500));
+
+window.addEventListener('load', () => {
+  const feedbackContent = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (feedbackContent) {
+    emailInput.value = feedbackContent.email;
+    messageInput.value = feedbackContent.message;
+  }
+});
+
+feedbackForm.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log({ email: emailInput.value, message: messageInput.value });
+    feedbackForm.reset();
+    localStorage.removeItem('feedback-form-state');
+});
